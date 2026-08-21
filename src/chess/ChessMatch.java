4ac295d3e23b54766f927a49cc4ebc948701a0a8
Caio Fernando Dias <caio.dias@sou.unifal-mg.edu.br>
2026-08-21 11:22:02 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,9 @@ public class ChessMatch {
         if (!board.thereIsPiece(position)) {
             throw new ChessException("There is no piece on position");
         }
+        if (!board.piece(position).isThereAnyPossibleMove()) {
+            throw new ChessException("There is no possible move for the selected piece");
+        }
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
